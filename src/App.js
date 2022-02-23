a6ea78887a51974d3da89284222c4317e5f53df7
timjancic <timjancic.gravity@gmail.com>
2022-02-23 17:03:11 -0600

    Starting layout with title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Plot from 'react-plotly.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+      <div className="App px-4 py-2 my-5 text-center">
+        <h1 className="display-5 fw-bold">
+          Foundry D&D 5e Stats
+        </h1>
+        <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">Quickly figure out who sucks at rolling in your D&D campaign</p>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" className="btn btn-primary btn-lg px-4 gap-3">Use your own data</button>
+      </div>
     </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
