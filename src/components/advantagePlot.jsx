846c1e5f62e1c3f512c65ed9d5aecc5c0cc97be7
timjancic <timjancic.gravity@gmail.com>
2022-03-02 22:21:24 -0600

    Added component that plots how often advantage was used

---
+++ src/components/advantagePlot.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Plot from "react-plotly.js";
+
+class AdvantagePlot extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <Plot
+          data={this.createTraces()}
+          layout={{
+            width: 400,
+            height: 400,
+            title: "How often there was dis/advantage",
+            barmode: "stack",
+            yaxis: {
+              title: {
+                text: "How many times"
+              }
+            },
+          }}
+        />
+      </React.Fragment>
+    );
+  }
+
+  createTraces() {
+    let data = [];
+
+    for (let i = 0; i < this.props.pcNames.length; i++) {
+      data.push({
+        x: ["disadvantage","normal","advantage"],
+        y: [this.props.pcStats[i].meta.nDis,this.props.pcStats[i].meta.nNorm,this.props.pcStats[i].meta.nAdv],
+        name: this.props.pcNames[i],
+        type: "bar"
+      });
+    }
+
+    return data;
+  }
+}
+
+export default AdvantagePlot;
